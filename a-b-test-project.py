5364d1c61b7d71d26e3e493bb6a3a650b47211a6
John Coletta <johnbcoletta@gmail.com>
2017-12-12 12:14:22 -0500

    Set up import statements

---
+++ a-b-test-project.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+
+#We are setting the seed to assure you get the same answers on quizzes as we set up
+random.seed(42)
