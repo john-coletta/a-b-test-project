a70064e0d3d11dd2ce9be8738fcdb8e9b572c832
John Coletta <johnbcoletta@gmail.com>
2017-12-12 12:16:07 -0500

    Reading in csv data

--- a-b-test-project.py
+++ a-b-test-project.py
@@ -5,3 +5,10 @@ import matplotlib.pyplot as plt
 
 #We are setting the seed to assure you get the same answers on quizzes as we set up
 random.seed(42)
+
+ab_data = pd.read_csv('ab_data.csv')
+
+#See when new_page and treatment don't line up
+treat = ab_data.query('group == "treatment"')
+contr = ab_data.query('group == "control"')
+print(contr['landing_page'].value_counts(), treat['landing_page'].value_counts())
